d0edea75e49260b1ed207db1fd8a92be5d6211fa
paulelong <paulelong@outlook.com>
2018-11-09 21:51:37 -0500

    AUdio format utilty

---
+++ MyMixes/MyMixes/MusicUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MyMixes
+{
+    static class MusicUtils
+    {
+        static public bool isAudioFormat(string filename)
+        {
+            switch (Path.GetExtension(filename))
+            {
+                case ".wav":
+                case ".mp3":
+                case ".wma":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
