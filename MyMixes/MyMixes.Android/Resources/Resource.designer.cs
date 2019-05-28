733048bd0451cb17780a2d08da882914f365cb7b
Paul E. Long <paulelong@outlook.com>
2019-05-28 12:13:22 -0400

    Android splash screen resolutions

--- MyMixes/MyMixes.Android/Resources/Resource.designer.cs
+++ MyMixes/MyMixes.Android/Resources/Resource.designer.cs
@@ -5914,26 +5914,26 @@ namespace MyMixes.Droid
 			// aapt resource value: 0x7f020058
 			public const int avd_hide_password = 2130837592;
 			
-			// aapt resource value: 0x7f020155
-			public const int avd_hide_password_1 = 2130837845;
+			// aapt resource value: 0x7f020158
+			public const int avd_hide_password_1 = 2130837848;
 			
-			// aapt resource value: 0x7f020156
-			public const int avd_hide_password_2 = 2130837846;
+			// aapt resource value: 0x7f020159
+			public const int avd_hide_password_2 = 2130837849;
 			
-			// aapt resource value: 0x7f020157
-			public const int avd_hide_password_3 = 2130837847;
+			// aapt resource value: 0x7f02015a
+			public const int avd_hide_password_3 = 2130837850;
 			
 			// aapt resource value: 0x7f020059
 			public const int avd_show_password = 2130837593;
 			
-			// aapt resource value: 0x7f020158
-			public const int avd_show_password_1 = 2130837848;
+			// aapt resource value: 0x7f02015b
+			public const int avd_show_password_1 = 2130837851;
 			
-			// aapt resource value: 0x7f020159
-			public const int avd_show_password_2 = 2130837849;
+			// aapt resource value: 0x7f02015c
+			public const int avd_show_password_2 = 2130837852;
 			
-			// aapt resource value: 0x7f02015a
-			public const int avd_show_password_3 = 2130837850;
+			// aapt resource value: 0x7f02015d
+			public const int avd_show_password_3 = 2130837853;
 			
 			// aapt resource value: 0x7f02005a
 			public const int DelBt = 2130837594;
@@ -6634,11 +6634,11 @@ namespace MyMixes.Droid
 			// aapt resource value: 0x7f020142
 			public const int notification_icon_background = 2130837826;
 			
-			// aapt resource value: 0x7f020153
-			public const int notification_template_icon_bg = 2130837843;
+			// aapt resource value: 0x7f020156
+			public const int notification_template_icon_bg = 2130837846;
 			
-			// aapt resource value: 0x7f020154
-			public const int notification_template_icon_low_bg = 2130837844;
+			// aapt resource value: 0x7f020157
+			public const int notification_template_icon_low_bg = 2130837847;
 			
 			// aapt resource value: 0x7f020143
 			public const int notification_tile_bg = 2130837827;
@@ -6674,19 +6674,28 @@ namespace MyMixes.Droid
 			public const int RightSong = 2130837837;
 			
 			// aapt resource value: 0x7f02014e
-			public const int tooltip_frame_dark = 2130837838;
+			public const int Splash = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int tooltip_frame_light = 2130837839;
+			public const int SplashProtoCfull = 2130837839;
 			
 			// aapt resource value: 0x7f020150
-			public const int TrashBt = 2130837840;
+			public const int SplashScreen = 2130837840;
 			
 			// aapt resource value: 0x7f020151
-			public const int UpFolderButton = 2130837841;
+			public const int tooltip_frame_dark = 2130837841;
 			
 			// aapt resource value: 0x7f020152
-			public const int UpSongBt = 2130837842;
+			public const int tooltip_frame_light = 2130837842;
+			
+			// aapt resource value: 0x7f020153
+			public const int TrashBt = 2130837843;
+			
+			// aapt resource value: 0x7f020154
+			public const int UpFolderButton = 2130837844;
+			
+			// aapt resource value: 0x7f020155
+			public const int UpSongBt = 2130837845;
 			
 			static Drawable()
 			{
